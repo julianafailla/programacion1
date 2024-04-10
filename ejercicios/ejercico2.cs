72d2aac4f6015b79a671787951818a82239af6f3
julianafailla <faillajuliana8@gmail.com>
2024-04-10 19:51:07 -0300

    commite 4
    c

---
+++ ejercicios/ejercico2.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("ingrese un numero ");
+int numero;
+if (!Int32.TryParse(Console.ReadLine(), out numero))
+{
+    Console.WriteLine("Error, debe ingresar un número correcto");
+}
+else
+{
+    numero %= 2;
+    if (numero == 0)
+    {
+        Console.WriteLine("el numero es par");
+    }
+    else
+    {
+        Console.WriteLine("el numero es impar");
+    }
+}
